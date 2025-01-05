c6e0b4d883f535720702bd1cba2fe3403f7ee6cf
Mohcin Kasmi <mkay@Manals-Macbook.local>
2025-01-06 00:23:31 +0100

    Add initial website files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Bot Command List page loaded!");
+    // You can add more dynamic functionality here, like tooltips or command interactions
+});
